18b49133676cc8e634db14cb293bf373d56a0657
Vlad Ionescu <ecstasyhoney@gmail.com>
2020-08-15 23:33:52 +0300

    Update dracula.py

--- dracula.py
+++ dracula.py
@@ -7,8 +7,9 @@ from moviepy.video.tools.cuts import find_video_period
 
 os.system("youtube-dl Fn70cJQ3EPY -o Dracula.mkv")  
 
-
+#importing the video and making the cut from minute 2:44 frame 7 to minute 2:48
 clip = (VideoFileClip("./Dracula.mkv", audio=False)
     .subclip((2,44.7),(2,48)))    
 
+#exporting the gif
 clip.write_gif('1.gif')
